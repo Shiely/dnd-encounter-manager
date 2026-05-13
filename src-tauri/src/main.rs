fdadf25c7f3fa6986350f2c45e8543fa15a6de63
james shiely <jpshiely@yahoo.com>
2026-05-12 22:09:50 -0700

    phase 1 complete. all 11 tests apss

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    dnd_encounter_manager_lib::run()
+    tauri::Builder::default().run(tauri::generate_context!()).expect("error while running tauri application")
 }
